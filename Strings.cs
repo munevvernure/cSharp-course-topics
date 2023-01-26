b28a8f721675594ae376c7c948aecc2c48b92fbc
munevvernure <116505664+munevvernure@users.noreply.github.com>
2023-01-26 18:18:41 +0300

    Add files via upload

---
+++ Strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            intro();
+        }
+
+        private static void intro()
+        {
+            //strings are actuallly a char array
+            string city = "Ankara";
+            // Console.WriteLine(city[0]);
+            foreach (char letter in city)
+            {
+                Console.WriteLine(letter);
+            }
+            string city2 = "Istanbul";
+            //writing two strings together
+            //string result = city + city2; intead of occupying place at memory
+            //it is more useful to use string.format class
+
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+            Console.ReadLine();
+        }
+    }
+}
